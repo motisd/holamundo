fdffe9dc00075fe7518b34348a2f93e62b1542ae
Motahare <motsai@alu.edu.gva.es>
2023-01-28 15:49:37 +0100

    mi primer commit

---
+++ eclipse-workspace/Entornos/src/holamundo/hola.java
@@ -0,0 +1,11 @@
+package holamundo;
+
+public class hola {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hola mundo");
+
+	}
+
+}
---
+++ eclipse-workspace/Entornos_Git/src/Entornos/Hola.java
@@ -0,0 +1,10 @@
+package Entornos;
+
+public class Hola {
+
+	public static void main(String[] args) {
+		System.out.println("Hola mundo");
+
+	}
+
+}
